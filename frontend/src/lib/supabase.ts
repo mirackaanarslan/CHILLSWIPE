2a1d084f0997623b9becb6a804222f806ffcbb07
mirackaanarslan <158054940+mirackaanarslan@users.noreply.github.com>
2025-07-13 15:09:23 +0200

    Update supabase.ts
    Make the supabase key secret.

--- frontend/src/lib/supabase.ts
+++ frontend/src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://mympcultccsrokpjpsdm.supabase.co'
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im15bXBjdWx0Y2Nzcm9rcGpwc2RtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIwNjc2OTYsImV4cCI6MjA2NzY0MzY5Nn0.-LDpmeaMnkO2QUix3IdyUAHXkVq3JVezkcuCGSdF6to'
+const supabaseUrl = SECRET
+const supabaseKey = SECRET
  
 export const supabase = createClient(supabaseUrl, supabaseKey) 
